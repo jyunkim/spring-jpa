f5cece38eb97c1c4a8e590911f64d2cec0c49f2f
jyunkim <jihyun.kim@bucketplace.net>
2021-09-30 17:37:46 +0900

    feat: Add value type collection

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -5,7 +5,9 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter @Setter
@@ -19,6 +21,17 @@ public class Member extends BaseEntity {
 
     @Embedded
     private Address address;
+
+    @ElementCollection
+    @CollectionTable(name = "address",
+            joinColumns = @JoinColumn(name = "member_id"))
+    private List<Address> addressHistory = new ArrayList<>();
+
+    @ElementCollection
+    @CollectionTable(name = "favorite_food",
+            joinColumns = @JoinColumn(name = "member_id"))
+    @Column(name = "food_name")
+    private Set<String> favoriteFoods = new HashSet<>();
 
     @OneToMany(mappedBy = "member") // 연관관계 주인이 아님을 명시. Order 엔티티의 member 필드에 의해 매핑
     private List<Order> orders = new ArrayList<>(); // 컬렉션은 필드에서 초기화하는 것이 null 문제에서 안전
