0e149a0dc291518b4eb67aec9712f1e9bfd8bd39
jyunkim <jihyun.kim@bucketplace.net>
2021-10-06 17:40:20 +0900

    feat: add constructor with member entity

--- src/main/java/jpabook/jpashop/dto/MemberDto.java
+++ src/main/java/jpabook/jpashop/dto/MemberDto.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop.dto;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Team;
 import lombok.Getter;
 
 @Getter
@@ -7,9 +9,16 @@ public class MemberDto {
 
     private Long id;
     private String name;
+    private Team team;
 
     public MemberDto(Long id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public MemberDto(Member member) {
+        id = member.getId();
+        name = member.getName();
+        team = member.getTeam();
     }
 }
