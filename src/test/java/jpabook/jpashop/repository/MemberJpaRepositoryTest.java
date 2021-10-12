b3cd9822841cd353a5c815ebd47a3208b98a059a
jyunkim <jihyun.kim@bucketplace.net>
2021-10-12 16:32:17 +0900

    test: add search test

--- src/test/java/jpabook/jpashop/repository/MemberJpaRepositoryTest.java
+++ src/test/java/jpabook/jpashop/repository/MemberJpaRepositoryTest.java
@@ -1,6 +1,9 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Team;
+import jpabook.jpashop.dto.MemberSearchCondition;
+import jpabook.jpashop.dto.MemberTeamDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,16 +21,16 @@ import static org.assertj.core.api.Assertions.*;
 // @Commit -> DB에 트랜잭션 커밋. 디폴트 값은 롤백
 public class MemberJpaRepositoryTest {
 
-    @Autowired
-    MemberJpaRepository memberJpaRepository;
+    @Autowired MemberJpaRepository memberJpaRepository;
+    @Autowired TeamRepository teamRepository;
 
-    @Test // JUnit4 - org.junit.Test
+    @Test // JUnit4 - org.junit.Test -> 메소드는 반드시 public이어야 함
     public void testMember() {
         Member member = new Member();
         member.setName("memberA");
 
         memberJpaRepository.save(member);
-        Member findMember = memberJpaRepository.findOne(1L);
+        Member findMember = memberJpaRepository.findById(1L);
 
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember).isEqualTo(member); // JPA 엔티티 동일성 보장
@@ -56,5 +59,70 @@ public class MemberJpaRepositoryTest {
 
         assertThat(members.size()).isEqualTo(3);
         assertThat(count).isEqualTo(5);
+    }
+
+    @Test
+    public void basicQuerydsl() {
+        Member member = new Member();
+        member.setName("member1");
+        member.setAge(10);
+        memberJpaRepository.save(member);
+
+        List<Member> result1 = memberJpaRepository.findAll_Querydsl();
+        assertThat(result1).containsExactly(member);
+
+        List<Member> result2 = memberJpaRepository.findByName_Querydsl("member1");
+        assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    public void searchTest() {
+        Team t1 = new Team();
+        t1.setName("team1");
+        teamRepository.save(t1);
+
+        Team t2 = new Team();
+        t2.setName("team2");
+        teamRepository.save(t2);
+
+        Member m1 = new Member();
+        m1.setName("member1");
+        m1.setAge(10);
+        m1.setTeam(t1);
+        memberJpaRepository.save(m1);
+
+        Member m2 = new Member();
+        m2.setName("member2");
+        m2.setAge(20);
+        m2.setTeam(t2);
+        memberJpaRepository.save(m2);
+
+        Member m3 = new Member();
+        m3.setName("member3");
+        m3.setAge(20);
+        m3.setTeam(t1);
+        memberJpaRepository.save(m3);
+
+        Member m4 = new Member();
+        m4.setAge(25);
+        m4.setTeam(t1);
+        memberJpaRepository.save(m4);
+
+        Member m5 = new Member();
+        m5.setName("member5");
+        m5.setAge(25);
+        m5.setTeam(t2);
+        memberJpaRepository.save(m5);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setTeamName("team1");
+        condition.setAgeGoe(20);
+        condition.setAgeLoe(24);
+
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+        assertThat(result).extracting("username").containsExactly("member3");
+
+        List<MemberTeamDto> result2 = memberJpaRepository.search(condition);
+        assertThat(result2).extracting("username").containsExactly("member3");
     }
 }
