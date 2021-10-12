0c07255516d2dc1edcb234c7a84cf2a81c242d34
jyunkim <jihyun.kim@bucketplace.net>
2021-10-12 16:31:55 +0900

    feat: add search method

--- src/main/java/jpabook/jpashop/repository/MemberJpaRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberJpaRepository.java
@@ -1,19 +1,37 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.QMember;
+import jpabook.jpashop.domain.QTeam;
 import jpabook.jpashop.dto.MemberDto;
+import jpabook.jpashop.dto.MemberSearchCondition;
+import jpabook.jpashop.dto.MemberTeamDto;
+import jpabook.jpashop.dto.QMemberTeamDto;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
+import static jpabook.jpashop.domain.QMember.*;
+import static jpabook.jpashop.domain.QTeam.*;
+
 @Repository
 public class MemberJpaRepository {
 
-    // 스프링 부트에서 자동 주입
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+
+    // main 함수가 있는 클래스에 빈으로 등록하고 주입받아도 됨
+    public MemberJpaRepository(EntityManager em) {
+        this.em = em;
+        this.queryFactory = new JPAQueryFactory(em);
+    }
 
     public void save(Member member) {
         // 영속성 컨텍스트에 Member 객체 삽입(PK 생성)
@@ -26,14 +44,21 @@ public class MemberJpaRepository {
     }
 
     // Primary key로 객체 찾음
-    public Member findOne(Long id) {
+    public Member findById(Long id) {
         return em.find(Member.class, id);
     }
 
     public List<Member> findAll() {
         // JPQL - 엔티티를 대상으로 쿼리 수행
         // 두번째 인자 = 반환 클래스
-        return em.createQuery("select m from Member m", Member.class).getResultList();
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findAll_Querydsl() {
+        return queryFactory
+                .selectFrom(member)
+                .fetch();
     }
 
     public List<Member> findByName(String name) {
@@ -43,8 +68,15 @@ public class MemberJpaRepository {
                 .getResultList();
     }
 
+    public List<Member> findByName_Querydsl(String name) {
+        return queryFactory
+                .selectFrom(member)
+                .where(member.name.eq(name))
+                .fetch();
+    }
+
     public List<MemberDto> findNameAndAge() {
-        return em.createQuery("select new jpabook.jpashop.dto.MemberDto(m.id, m.name) from Member m", MemberDto.class)
+        return em.createQuery("select new jpabook.jpashop.dto.MemberDto(m.name, m.age) from Member m", MemberDto.class)
                 .getResultList();
     }
 
@@ -58,5 +90,72 @@ public class MemberJpaRepository {
                 .setFirstResult(offset)
                 .setMaxResults(limit)
                 .getResultList();
+    }
+
+    public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        // null뿐만 아니라 "" 같이 빈 문자열도 체크
+        if (StringUtils.hasText(condition.getUsername())) {
+            builder.and(member.name.eq(condition.getUsername()));
+        }
+        if (StringUtils.hasText(condition.getTeamName())) {
+            builder.and(team.name.eq(condition.getTeamName()));
+        }
+        if (condition.getAgeGoe() != null) {
+            builder.and(member.age.goe(condition.getAgeGoe()));
+        }
+        if (condition.getAgeLoe() != null) {
+            builder.and(member.age.loe(condition.getAgeLoe()));
+        }
+
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id,
+                        member.name,
+                        member.age,
+                        team.id,
+                        team.name
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(builder)
+                .fetch();
+    }
+
+    public List<MemberTeamDto> search(MemberSearchCondition condition) {
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id,
+                        member.name,
+                        member.age,
+                        team.id,
+                        team.name
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return StringUtils.hasText(username) ? member.name.eq(username) : null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return StringUtils.hasText(teamName) ? team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe != null ? member.age.loe(ageLoe) : null;
     }
 }
