7a04e3490bfc94ac96129c41f77ceecd528b2ba9
jyunkim <jihyun.kim@bucketplace.net>
2021-10-12 14:06:41 +0900

    feat: add queryProjection

--- src/main/java/jpabook/jpashop/dto/MemberDto.java
+++ src/main/java/jpabook/jpashop/dto/MemberDto.java
@@ -1,24 +1,30 @@
 package jpabook.jpashop.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Team;
-import lombok.Getter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
+@Data
+@NoArgsConstructor
 public class MemberDto {
 
     private Long id;
     private String name;
+    private int age;
     private Team team;
 
-    public MemberDto(Long id, String name) {
-        this.id = id;
+    @QueryProjection
+    public MemberDto(String name, int age) {
         this.name = name;
+        this.age = age;
     }
 
     public MemberDto(Member member) {
         id = member.getId();
         name = member.getName();
+        age = member.getAge();
         team = member.getTeam();
     }
 }
