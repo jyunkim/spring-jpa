7a2adcd21a1175b2bbfbb9f4ec9ff694133bb253
jyunkim <jihyun.kim@bucketplace.net>
2021-10-12 16:31:07 +0900

    feat: add constructor

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -40,6 +40,14 @@ public class Member extends BaseTimeEntity {
     @OneToMany(mappedBy = "member") // 연관관계 주인이 아님을 명시. Order 엔티티의 member 필드에 의해 매핑
     private List<Order> orders = new ArrayList<>(); // 컬렉션은 필드에서 초기화하는 것이 null 문제에서 안전
 
+    public Member() {}
+
+    public Member(String name, int age, Team team) {
+        this.name = name;
+        this.age = age;
+        this.team = team;
+    }
+
     @Override
     public String toString() {
         return "Member{" +
